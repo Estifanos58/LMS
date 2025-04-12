a8338270b882fa05dd5b2943a382cf8b8d5362a8
Estifanos58 <estifkebe08@gmail.com>
2025-04-12 15:08:00 +0300

    add: nav bar and left side bar added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +18,20 @@ export default function DashbordLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>{children}</div>
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:[8%] xl:w-[14%] p-4">
+        <Link href={"/"} className="flex items-center justify-center lg:justify-start gap-2">
+        <Image src={"/logo.png"} alt="Logo" width={32} height={32}/>
+        <span className="hidden lg:block">School Management</span>
+        </Link>
+        <Menu/>
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar/>
+        {children}
+      </div>
+    </div>
   );
 }
