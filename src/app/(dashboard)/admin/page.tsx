264ce32c598af74a3db350921ca778870a96c169
Estifanos58 <estifkebe08@gmail.com>
2025-04-13 12:14:13 +0300

    update: admin Ui finished

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/components/Announcements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CounteChart from "@/components/CounteChart"
 import EventCalender from "@/components/EventCalender"
@@ -36,6 +37,7 @@ const AdminPage = () => {
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalender/>
+        <Announcements/>
       </div>
     </div>
   )
