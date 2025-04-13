c965fb72d75695a459b8ef33db5287aa135cd6ae
Estifanos58 <estifkebe08@gmail.com>
2025-04-13 19:34:32 +0300

    progress: table for the teachersList in the making

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,7 +1,93 @@
+import Pagination from "@/components/Pagination"
+import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
+import { role, teachersData } from "@/lib/data"
 import Image from "next/image"
+import Link from "next/link"
+
+type Teacher = {
+  id:number,
+  teacherId:string,
+  name:string,
+  photo:string
+  email?:string,
+  phone:string,
+  subjects:string[],
+  classes:string[],
+  address:string,
+}
+
+const column = [
+  {
+    header: "Info", 
+    accessor: "info",
+  },
+  {
+    header: "Teacher ID", 
+    accessor: "teacherId", 
+    className: "hidden md:table-cell"
+  },
+  {
+    header: "Subjects", 
+    accessor: "sujects", 
+    className: "hidden md:table-cell"
+  },
+  {
+    header:"Classes", 
+    accessor: "classes",
+    className:"hidden md:table-cell"
+  },
+  {
+    header:"Phone", 
+    accessor: "phone",
+    className:"hidden lg:table-cell"
+  },
+  {
+    header:"Address", 
+    accessor: "address",
+    className:"hidden lg:table-cell"
+  },
+  {
+    header:"Actions", 
+    accessor: "action",
+  },
+]
+
 
 const TeacherList = () => {
+
+  const renderRow = ((item:Teacher)=> (
+    <tr key={item.id}>
+      <td>
+        <Image src={item.photo} alt={item.name} width={40} height={40} className="md:hidden lg:block rounded-full object-cover"/>
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+      <td className="hidden md:table-cell">{item.classes.join(',')}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src={'/view.png'} alt="" width={16} height={16}/>
+            </button>
+            </Link>
+            {role === "admin" && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              <Image src={'/delete.png'} alt="" width={16} height={16}/>
+            </button>
+            )}
+        </div>
+      </td>
+
+    </tr>
+  ))
+
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
       {/* TOP */}
@@ -11,25 +97,23 @@ const TeacherList = () => {
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src={'/filter.png'} alt="" width={14} height={14}/>
+              <Image src={'/filter.png'} alt="" width={14} height={14} />
             </button>
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src={'/sort.png'} alt="" width={14} height={14}/>
+              <Image src={'/sort.png'} alt="" width={14} height={14} />
             </button>
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src={'/plus.png'} alt="" width={14} height={14}/>
+              <Image src={'/plus.png'} alt="" width={14} height={14} />
             </button>
           </div>
         </div>
       </div>
       {/* LIST */}
       <div className="">
-
+        <Table columns={column} renderRow={renderRow} data={teachersData}/>
       </div>
       {/* PAGINATION */}
-      <div className="">
-
-      </div>
+      <Pagination />
     </div>
   )
 }
