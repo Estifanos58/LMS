153421a548ee734d0e2542ec08bf5d2211f6f590
Estifanos58 <estifkebe08@gmail.com>
2025-04-14 16:35:46 +0300

    update:code refactor in the formmodal

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -72,9 +73,7 @@ const StudentList = () => {
             </button>
             </Link>
             {role === "admin" && (
-              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-              <Image src={'/delete.png'} alt="" width={16} height={16}/>
-            </button>
+              <FormModal table="student" type="delete" id={item.id}/>
             )}
         </div>
       </td>
@@ -96,10 +95,8 @@ const StudentList = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src={'/sort.png'} alt="" width={14} height={14} />
             </button>
-           {role === "admin" && <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src={'/plus.png'} alt="" width={14} height={14} />
-            </button>
-            }
+           {role === "admin" && <FormModal table="student" type="create" />}
+            
           </div>
         </div>
       </div>
