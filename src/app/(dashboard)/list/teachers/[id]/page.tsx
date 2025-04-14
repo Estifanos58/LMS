5f0c3dfb5a7ed7c7ed0ccca805dfb436e2689d3e
Estifanos58 <estifkebe08@gmail.com>
2025-04-14 13:29:33 +0300

    add:single teacher page complete

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,10 +1,14 @@
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalendar"
+import Performance from "@/components/Performance"
 import Image from "next/image"
+import Link from "next/link"
 
 const SingleTeacherPage = () => {
   return (
     <div className='flex-1 flex p-4 flex-col gap-4 xl:flex-row'>
       {/* LEFT */}
-      <div className="w-full xl:2-2/3">
+      <div className="w-full xl:w-2/3">
         {/* TOP */}
         <div className="flex flex-col lg:flex-row gap-4">
             {/* USER INFO CARD */}
@@ -36,17 +40,61 @@ const SingleTeacherPage = () => {
               </div>
             </div>
             {/* SMALL CARDS */}
-            <div className="flex-1">
+            <div className="flex-1 flex gap-4 justify-between flex-wrap ">
+              {/* CARD */}
+              <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                  <Image src={'/singleAttendance.png'} alt="" width={24} height={24} className="w-6 h-6"/>
+                  <div className="">
+                      <h1 className="text-xl font-semibold">90%</h1>
+                      <span className="text-sm text-gray-400">Attendance</span>
+                  </div>
+              </div>
+               {/* CARD */}
+               <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                  <Image src={'/singleBranch.png'} alt="" width={24} height={24} className="w-6 h-6"/>
+                  <div className="">
+                      <h1 className="text-xl font-semibold">2</h1>
+                      <span className="text-sm text-gray-400">Branches</span>
+                  </div>
+              </div>
+               {/* CARD */}
+               <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                  <Image src={'/singleLesson.png'} alt="" width={24} height={24} className="w-6 h-6"/>
+                  <div className="">
+                      <h1 className="text-xl font-semibold">9</h1>
+                      <span className="text-sm text-gray-400">Lessons</span>
+                  </div>
+              </div>
+               {/* CARD */}
+               <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                  <Image src={'/singleClass.png'} alt="" width={24} height={24} className="w-6 h-6"/>
+                  <div className="">
+                      <h1 className="text-xl font-semibold">6</h1>
+                      <span className="text-sm text-gray-400">Classes</span>
+                  </div>
+              </div>
             </div>
         </div>
         {/* BOTTOM */}
-        <div className="">
-
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Teacher&apos;s Schedule</h1>
+          <BigCalendar/>
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full xl:w-1/3">
-
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+      <div className="bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold">Shortcuts</h1>
+        <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+          <Link className="p-3 rounded-md bg-lamaSkyLight" href={'/'}>Teacher&apos;s Classes</Link>
+          <Link className="p-3 rounded-md bg-lamaPurpleLight" href={'/'}>Teacher&apos;s Students</Link>
+          <Link className="p-3 rounded-md bg-lamaYellowLight" href={'/'}>Teacher&apos;s Lessons</Link>
+          <Link className="p-3 rounded-md bg-pink-50" href={'/'}>Teacher&apos;s Exams</Link>
+          <Link className="p-3 rounded-md bg-lamaSkyLight" href={'/'}>Teacher&apos;s Assignments</Link>
+        </div>
+      </div>
+        <Performance/>
+        <Announcements/>
       </div>
     </div>
   )
